48187812db138d905e8090ca727260f201349663
zhenggao cai <caizhengao1992@gmail.com>
2019-04-19 05:55:00 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,5 +6,10 @@ block_return_values = []
 while i < collection.length 
 block_return_values << yield(collection[i])
 i += 1 
+end
+if block_return_values.include?(false)
+  false 
+else 
+  true 
 end 
 end
